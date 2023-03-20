28ed3fc6dd5abdf17a84c4435e359711b1d54a95
Geun Han Chung <97194224+biggestUaena24@users.noreply.github.com>
2023-03-20 17:00:34 -0400

    Update julia_set.c
    Changed in calculating the local variables for start and end row to see if it is working or not

--- julia_set.c
+++ julia_set.c
@@ -56,8 +56,11 @@ int main(int argc, char **argv) {
     double complex c = -0.8 + 0.156 * I;
     int rows_per_process = HEIGHT / size;
     // (rank-1) * rows_per_process + min((rank-1), (HEIGHT % size - 1))
-    int start_row = (rank-1) * rows_per_process + min((rank-1), (HEIGHT % (size - 1)));
-    int end_row = (rank) * rows_per_process + min((rank), (HEIGHT % (size - 1))) - 1;
+    int start_row = rank * rows_per_process;
+    int end_row = start_row + rows_per_process;
+    if (rank == size - 1) {
+        end_row = HEIGHT;
+    }
 
     generate_julia_set_section(image, start_row, end_row, c);
 
